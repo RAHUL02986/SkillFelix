4a1fd5f162b0285aaf4d68ead59d4bf5419c1fb1
rahul02986 <kapoorrahul304@gmail.com>
2025-06-28 17:25:04 +0530

    Initial commit to SkillFelix repo

---
+++ .next/server/app/api/subscribeNewsletter/route_client-reference-manifest.js
@@ -0,0 +1,2 @@
+globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
+globalThis.__RSC_MANIFEST["/api/subscribeNewsletter/route"] = {"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
